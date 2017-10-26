57d5c04a337369655f54b77bbdba431aac11a4c9
zz <zz@zzdeMac-mini.local>
2017-10-26 17:36:34 +0800

    autolayout.framework

---
+++ Sources/Auto_Layout.h
@@ -0,0 +1,19 @@
+//
+//  Auto_Layout.h
+//  Auto_Layout
+//
+//  Created by zz on 26/10/2017.
+//  Copyright © 2017 zzkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Auto_Layout.
+FOUNDATION_EXPORT double Auto_LayoutVersionNumber;
+
+//! Project version string for Auto_Layout.
+FOUNDATION_EXPORT const unsigned char Auto_LayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Auto_Layout/PublicHeader.h>
+
+
